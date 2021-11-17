a508c9ba0e64dbbad2081cfa04e8de263be43074
bjorneju <bjorneju@gmail.com>
2021-11-17 10:50:24 -0600

    not sure what I changed

--- visualize_pyphi/__init__.py
+++ visualize_pyphi/__init__.py
@@ -1,7 +1,9 @@
 from . import visualize_ces as viz
 from . import simulate_ces as sim
 from . import special_plots as plots
+from . import network_generator as ng
 from . import compute
+
 
 import pyphi
 
